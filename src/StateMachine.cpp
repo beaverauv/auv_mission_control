3d2ae712e9ce2414d83ff07f470cae38ede63039
Seth <isaacson17@bcdschool.org>
2018-06-23 23:13:40 -0700

    fixed crap formatting

--- src/StateMachine.cpp
+++ src/StateMachine.cpp
@@ -21,123 +21,120 @@ PidManager pm(&nh);
 Camera cam;
 TaskVision vision(&cam);
 
-  while(ros::ok){ //careful
-  if(timerStarted)
-    timeSinceStart = mainTimer.getTime();
+while(ros::ok){ //careful
+if(timerStarted)
+timeSinceStart = mainTimer.getTime();
 
-    switch(currentState){
-      case 0: {//init
-        if(initCount < 1){
-          ROS_INFO("EXECUTING INIT STATE");
-          initCount++;
-        }
-        //calibrate sensors
-        if(killSwitch){
-          currentState = 9;
-        }
-        else if(pm.getStart() == true){
-          currentState = 1;
-          ROS_INFO("STARTING");
-          mainTimer.start();
-          timerStarted = true;
-          while(mainTimer.getTime() < 1){
-            ros::spinOnce;
-            stateRate.sleep();
-          }
-	}
-        else
-          currentState = 0;
-        break;
-      }
-
-
-      case 1: { //gate
-        ROS_INFO("EXECUTING GATE TASK");
-        TaskGate gate(&pm, &cam, &vision);
-	int outcome = gate.execute();
-       // ROS_INFO("outcome %d", outcome);
-
-        if (outcome == succeeded){
-          currentState = 2;
-	  ROS_INFO("Gate task succeeded. Transitioning to Buoy task");
-        }
-	else if (outcome == timeout)// || getTimeout())
-          currentState = 8;
-        else if (outcome == kill){
-          currentState = 9;
-          ROS_INFO("kill");
-        }
-        else
-          currentState = 9;
-        break;
-
-      }
-      case 2: { //buoy
-
-        ROS_INFO("EXECUTING BUOY TASK");
-        TaskBuoy buoy(&pm, &vision);
-        //int outcome = 10;
-	int outcome =  buoy.execute();
-        //ROS_INFO("outcome %d", outcome);
-
-        if (outcome == succeeded){
-          currentState = 10;
-          ROS_INFO("Buoy task succeeded. Resetting to init state");
-        }
-	else if (outcome == timeout || getTimeout())
-          currentState = 8;
-        else if (outcome == kill || pm.getKill()){
-          currentState = 9;
-          ROS_INFO("kill");
-        }
-        else
-          currentState = 9;
-        break;
-
-      }
-
-
-
-      case 9:{ //kill
-        //stop thrusters (in resource file)
-        break;
-      }
-
-      case 10:{//prep to go back to init
-
-        if(initCount < 1){
-          ROS_INFO("All QUEUED TASKS COMPLETED. RETURN START SWITCH TO RETURN TO INIT STATE.");
-          initCount++;
-        }
-        //calibrate sensors
-        if(killSwitch){
-          currentState = 9;
-        }
-        else if(pm.getStart() == false){
-          currentState = 0;
-          initCount = 0;
-          ROS_INFO("RETURNING TO INIT STATE");
-        }
-        else
-          currentState = 10;
-        break;
-      }
-
-      default:
-        break;//NO
-
+switch(currentState){
+  case 0: {//init
+    if(initCount < 1){
+      ROS_INFO("EXECUTING INIT STATE");
+      initCount++;
     }
-
-    ros::spinOnce();
-    stateRate.sleep();
+    //calibrate sensors
+    if(killSwitch){
+      currentState = 9;
+    }
+    else if(pm.getStart()){
+      currentState = 1;
+      ROS_INFO("STARTING");
+      mainTimer.start();
+      timerStarted = true;
+      while(mainTimer.getTime() < 1){
+        ros::spinOnce;
+        stateRate.sleep();
+      }
+    }
+    else
+      currentState = 0;
+    break;
   }
+
+
+  case 1: { //gate
+    ROS_INFO("EXECUTING GATE TASK");
+    TaskGate gate(&pm, &cam, &vision);
+    int outcome = gate.execute();
+    // ROS_INFO("outcome %d", outcome);
+
+    if (outcome == succeeded){
+      currentState = 2;
+      ROS_INFO("Gate task succeeded. Transitioning to Buoy task");
+    }
+    else if (outcome == timeout)// || getTimeout())
+      currentState = 8;
+    else if (outcome == kill){
+      currentState = 9;
+      ROS_INFO("kill");
+    }
+    else
+      currentState = 9;
+    break;
+  }
+  case 2: { //buoy
+
+    ROS_INFO("EXECUTING BUOY TASK");
+    TaskBuoy buoy(&pm, &vision);
+    //int outcome = 10;
+    int outcome =  buoy.execute();
+    //ROS_INFO("outcome %d", outcome);
+    if (outcome == succeeded){
+      currentState = 10;
+      ROS_INFO("Buoy task succeeded. Resetting to init state");
+    }
+    else if (outcome == timeout || getTimeout())
+      currentState = 8;
+    else if (outcome == kill || pm.getKill()){
+      currentState = 9;
+      ROS_INFO("kill");
+    }
+    else
+      currentState = 9;
+    break;
+  }
+
+
+
+  case 9:{ //kill
+    //stop thrusters (in resource file)
+    break;
+  }
+
+  case 10:{//prep to go back to init
+
+    if(initCount < 1){
+      ROS_INFO("All QUEUED TASKS COMPLETED. RETURN START SWITCH TO RETURN TO INIT STATE.");
+      initCount++;
+    }
+    //calibrate sensors
+    if(killSwitch){
+      currentState = 9;
+    }
+    else if(!pm.getStart()){
+      currentState = 0;
+      initCount = 0;
+      ROS_INFO("RETURNING TO INIT STATE");
+    }
+    else
+      currentState = 10;
+    break;
+  }
+
+  default:
+    break;//NO
+
+}
+
+ros::spinOnce();
+stateRate.sleep();
+}
 
 }
 
 
 bool getTimeout(){
-  if (timeSinceStart <= 15 * 60)
-    return true;
-  else
-    return false;
+if (timeSinceStart <= 15 * 60)
+  return true;
+else
+  return false;
 }
